8805d56f77d020dac27a4cee9e948683fc913939
Onovo Christian <onovochristian19@gmail.com>
2024-12-24 20:05:05 +0100

    Added the Page layout for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 import HomePage from "./pages/HomePage"
+import MainLayout from "./layouts/MainLayout"
 
 function App() {
 
-  return (
-    <>
-      <HomePage />
-    </>
-  )
+    // Define the routes using createBrowserRouter 
+    const router = createBrowserRouter([
+      {
+        path: '/',
+        element: <MainLayout />,
+        children: [
+          { index: true, element: <HomePage />}
+        ]
+      }
+    ])
+
+    return <RouterProvider router={router}/>
 }
 
 export default App
