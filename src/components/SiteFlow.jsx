db3dcfdb086699400f7d28ecfdd8592b84778e51
Onovo Christian <onovochristian19@gmail.com>
2025-01-03 19:08:31 +0100

    Some edit

--- src/components/SiteFlow.jsx
+++ src/components/SiteFlow.jsx
@@ -9,14 +9,14 @@ const SiteFlow = () => {
 
   return (
     <div className="bg-gray-100 w-full p-4 pt-0 mt-0">
-      <h2 className="text-3xl font-bold text-center mt-4 mb-4 font-inter">How It Works</h2>
+      <h2 className="text-3xl font-bold text-center mt-8 mb-4 font-inter">How It Works</h2>
       <p className="text-center text-gray-700 mb-8">
         Follow these simple steps to find and apply for your dream job.
       </p>
       <div className="flex flex-col justify-center items-center text-center">
         <div className="flex flex-col md:flex-row items-center p-4 bg-primary-100 rounded-lg shadow-lg mb-6 w-full shadow-md" data-aos="fade-up">
           <div className="md:w-1/2 p-2">
-            <h3 className="text-2xl font-semibold mb-2 font-inter text-primary-700">1. Browse Jobs</h3>
+            <h3 className="text-2xl font-semibold mb-4 mt-4 font-inter text-primary-700">1. Browse Jobs</h3>
             <p className="text-gray-700 mb-2">
               Explore a wide range of job listings in various fields and industries. Our platform offers a comprehensive
               list of job opportunities from top companies. 
@@ -31,7 +31,7 @@ const SiteFlow = () => {
             <img src="https://media.istockphoto.com/id/1184240745/de/vektor/humane-resourcen-personen-und-zwischenablage-mit-bewerberliste-und-lupe-besch%C3%A4ftigung.jpg?s=612x612&w=0&k=20&c=sh5mC-_B-hOx1BqLuYWaf7QdmGxSYJfkfFYOC0u2UHs=" alt="Find Your Match" className="rounded-lg shadow-lg w-2/3 transition-transform duration-500 hover:scale-105" />
           </div>
           <div className="md:w-1/2 p-2 order-1 md:order-2">
-            <h3 className="text-2xl font-semibold mb-2 font-inter text-secondary-700">2. Find Your Match</h3>
+            <h3 className="text-2xl font-semibold mb-4 mt-4 font-inter text-secondary-700">2. Find Your Match</h3>
             <p className="text-gray-700 mb-2">
               Use our advanced search and filter options to find the perfect job that matches your skills and interests.
               Our intelligent matching system helps you discover jobs that align with your career goals.
@@ -40,7 +40,7 @@ const SiteFlow = () => {
         </div>
         <div className="flex flex-col md:flex-row items-center p-4 bg-accent-100 rounded-lg shadow-lg mb-6 w-full shadow-md" data-aos="fade-up">
           <div className="md:w-1/2 p-2">
-            <h3 className="text-2xl font-semibold mb-2 font-inter text-accent-700">3. Apply Directly</h3>
+            <h3 className="text-2xl font-semibold mb-4 mt-4 font-inter text-accent-700">3. Apply Directly</h3>
             <p className="text-gray-700 mb-2">
               Submit your application directly through our platform and get in touch with potential employers. Our seamless
               application process allows you to apply to multiple jobs with just a few clicks.
