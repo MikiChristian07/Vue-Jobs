8b7b54546b83ea5cf751ea6a99f564be66714c41
Onovo Christian <onovochristian19@gmail.com>
2025-01-08 22:18:58 +0100

    Initialiazied the home contact field

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import FeaturedJobs from '../components/FeaturedJobs';
 import SiteStats from '../components/SiteStats';
 import SiteFlow from '../components/SiteFlow';
 import PathChoice from '../components/PathChoice';
+import HomeContact from '../components/HomeContact';
 
 const HomePage = () => {
   return (
@@ -12,6 +13,7 @@ const HomePage = () => {
       <SiteStats />
       <SiteFlow />
       <PathChoice />
+      <HomeContact />
     </div>
   );
 };
